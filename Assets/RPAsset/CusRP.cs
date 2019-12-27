4f4c5986fd9b3772417b73912296a06ebe9f6df5
gaoze <gaoze@hundredcent.com>
2019-12-27 14:24:29 +0800

    Initial commit

---
+++ Assets/RPAsset/CusRP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CusRP : RenderPipeline
+{
+    #region var
+    public struct CusRPParam
+    {
+        public Color ClearColor;
+    }
+    CusRPParam m_param ;
+    #endregion 
+    #region public method
+    public void Setup(in CusRPParam param )
+    {
+        m_param = param;
+    }
+    #endregion
+    
+    #region inherit
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        //base.Render(context, cameras);
+        CommandBuffer cmd = new CommandBuffer();
+        cmd.Clear();
+        cmd.ClearRenderTarget(true, true, m_param.ClearColor);
+        context.ExecuteCommandBuffer(cmd);
+        context.Submit();
+        cmd.Release();
+    }
+    #endregion
+}
---
+++ Assets/RPAsset/CusRPAsset.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class CusRPAsset : RenderPipelineAsset
+{
+    #region public var
+    public Color ClearColor;
+    #endregion
+    
+    #region  private var
+    static string m_asset_path = @"Assets/RPAsset/CusRPAsset.asset";
+
+    //传递给CusRP的设置参数
+    
+    #endregion
+
+    #region method
+    [MenuItem("CustomRP/Create A New Custom RenderPipeline")]
+    static void CreateCusRPAsset()
+    {
+        RenderPipelineAsset rp_asset = new CusRPAsset();
+        AssetDatabase.CreateAsset(rp_asset, m_asset_path);
+    }
+    
+    void FillRPParam(ref CusRP.CusRPParam param)
+    {
+        param.ClearColor = ClearColor;
+    }
+    #endregion
+
+    #region inherited
+    protected override RenderPipeline CreatePipeline()
+    {
+        RenderPipeline rp = new CusRP();
+        CusRP.CusRPParam param = new CusRP.CusRPParam();
+        FillRPParam(ref param);
+        (rp as CusRP).Setup(param);
+        return rp;
+    }
+    #endregion
+}
